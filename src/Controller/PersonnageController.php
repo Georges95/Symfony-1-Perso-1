ef7f05854075bcaf00b7b3fbcbf76b59634075fd
georges <georges.laerche@gmail.com>
2022-01-08 19:20:59 +0100

    Base du site sans BDD

---
+++ src/Controller/PersonnageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonnageController extends AbstractController
+{
+    #[Route('/', name: 'accueil')]
+    public function index(): Response
+    {
+        return $this->render('personnage/index.html.twig');
+    }
+
+    #[Route('/persos', name: 'personnages')]
+    public function persos(): Response
+    {
+        return $this->render('personnage/persos.html.twig',[
+            "pseudo" => "toto",
+            "age" => 25,
+            "carac" => [
+                "force" => 3,
+                "agi" => 2,
+                "intel" => 3
+            ]
+
+        ]);
+    }
+}
