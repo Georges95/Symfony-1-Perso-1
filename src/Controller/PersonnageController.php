a1493de309c9af615df21ea3768bce2028981619
georges <georges.laerche@gmail.com>
2022-01-10 16:09:21 +0100

    Ajout des personnages

--- src/Controller/PersonnageController.php
+++ src/Controller/PersonnageController.php
@@ -17,15 +17,44 @@ class PersonnageController extends AbstractController
     #[Route('/persos', name: 'personnages')]
     public function persos(): Response
     {
-        return $this->render('personnage/persos.html.twig',[
-            "pseudo" => "toto",
+        $j1 = [
+            "nom" => "Marc",
             "age" => 25,
+            "sexe" => true,
             "carac" => [
                 "force" => 3,
                 "agi" => 2,
                 "intel" => 3
             ]
+        ];
+        $j2 = [
+            "nom" => "Milo",
+            "age" => 25,
+            "sexe" => true,
+            "carac" => [
+                "force" => 5,
+                "agi" => 1,
+                "intel" => 2
+            ]
+        ];
+        $j3 = [
+            "nom" => "Tya",
+            "age" => 25,
+            "sexe" => false,
+            "carac" => [
+                "force" => 1,
+                "agi" => 2,
+                "intel" => 5
+            ]
+        ];
+        $players=[
+            "j1" => $j1,
+            "j2" => $j2,
+            "j3" => $j3
+        ];
 
+        return $this->render('personnage/persos.html.twig',[
+           "players" => $players
         ]);
     }
 }
